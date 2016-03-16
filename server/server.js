5105b45cf2db02c49e7bc7dcb8f3e0b7bfa2c6c5
rfenolio <mordoc@gmail.com>
2016-03-16 11:20:05 -0700

    clean up server file and put request handlers in the proper file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var parser = require('body-parser');
 var cors = require('cors');
 var amazon = require('amazon-product-api');
+var handlers = require('./src/requesthandlers')
 
 
 var app = express();
@@ -13,35 +14,14 @@ app.use(cors());
 // serve the app from the client folder
 app.use(express.static(__dirname + '/../client'));
 
+// not getting anything on the base route yet
 app.get('/', function (req, res) {
 	
 	res.end();
 })
 
-app.post('/api/products', function (req, res) {
-
-	console.log(req.body);
-
-	var client = amazon.createClient({
-	  awsId: process.env.AWS_ACCESS_KEY_ID,
-	  awsSecret: process.env.AWS_SECRET_KEY,
-	  awsTag: process.env.ASSOCIATE_TAG
-	});
-
-	client.itemSearch({
-	  keywords: req.body.keywords,
-	  responseGroup: 'ItemAttributes,Offers,Images'
-	}, function(err, results, response) {
-	  if (err) {
-	    console.log(err);
-	  } else {
-	  	res.send(results);
-	  	res.end();
-	  }
-	});
-	
-	var dummyData = ['Hat', 'Book', 'Ant Traps', 'Cup']
-})
+// post search query for amazon, and return results
+app.post('/api/products', handlers.products);
 
 
 // start server
--- server/src/requestHandlers.js
+++ server/src/requestHandlers.js
@@ -1,33 +1,26 @@
+var amazon = require('amazon-product-api');
 
+module.exports.products = function (req, res) {
 
-// module.expports.products = function(req, res){
-// 	req.body
-// 	console.dir()
+	console.log(req.body);
 
-// }
+	var client = amazon.createClient({
+	  awsId: process.env.AWS_ACCESS_KEY_ID,
+	  awsSecret: process.env.AWS_SECRET_KEY,
+	  awsTag: process.env.ASSOCIATE_TAG
+	});
 
-// console.log(process.env.AWS_ACCESS_KEY_ID);
-
-// var util = require('util'),
-//     OperationHelper = require('apac').OperationHelper;
- 
-// var opHelper = new OperationHelper({
-//     awsId:     process.env.AWS_ACCESS_KEY_ID,
-//     awsSecret: process.env.AWS_SECRET_KEY,
-//     assocId:   process.env.ASSOCIATE_TAG
-//     // xml2jsOptions: an extra, optional, parameter for if you want to pass additional options for the xml2js module. (see https://github.com/Leonidas-from-XIV/node-xml2js#options) 
-// });
- 
- 
-// // execute(operation, params, callback) 
-// // operation: select from http://docs.aws.amazon.com/AWSECommerceService/latest/DG/SummaryofA2SOperations.html 
-// // params: parameters for operation (optional) 
-// // callback(err, parsed, raw): callback function handling results. err = potential errors raised from xml2js.parseString() or http.request(). parsed = xml2js parsed response. raw = raw xml response. 
- 
-// opHelper.execute('ItemSearch', {
-//   'SearchIndex': 'Books',
-//   'Keywords': 'harry potter',
-//   'ResponseGroup': 'ItemAttributes,Offers'
-// }, function(err, results) { // you can add a third parameter for the raw xml response, "results" here are currently parsed using xml2js 
-// 	console.log(results);
-// });
+	client.itemSearch({
+	  keywords: req.body.keywords,
+	  responseGroup: 'ItemAttributes,Offers,Images'
+	}, function(err, results, response) {
+	  if (err) {
+	    console.log(err);
+	  } else {
+	  	res.send(results);
+	  	res.end();
+	  }
+	});
+	
+	var dummyData = ['Hat', 'Book', 'Ant Traps', 'Cup']
+}
